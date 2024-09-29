55762de4c509bf9e273b7feeb211f60a737e190a
Anit K Peter <anitkp3k@gmail.com>
2024-09-29 12:16:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: 'MTI4OTYyMDQ3NjAxNTkzNTUyOQ.GpwQZv.RmdMCwedC5qUksBOfazzIlMx3cyRz055O_vy80NZ4',// Bot token    
-    clientId: '1289620476011235935529',// Bot client id
-    guildId: '1240546156577012330164',// Guild id
-    tweetChannelId: '1270052311234220335126',// message channel
+    token: 'bot token',// Bot token    
+    clientId: 'bot id',// Bot client id
+    guildId: 'server id',// Guild id
+    tweetChannelId: 'medsage send Channrl',// message channel
     prefix: '!'//bot prefix
 };
